ab076492fdb8e4071140f2ee3009d4504d12b8be
yessygc <yessgarciacampos@gmail.com>
2022-03-29 20:27:27 -0600

    Post Tweet

--- main.py
+++ main.py
@@ -234,8 +234,23 @@ def home():
             summary="Post a Tweet",
             tags=["Tweets"]
         )
-def post_tweet():
-    pass
+def post_tweet(tweet: Tweet = Body(...)):
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_user_dict = tweet_dict["by"]
+
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["update_at"] = str(tweet_dict["update_at"])
+        tweet_user_dict["user_id"] = str(tweet_user_dict["user_id"])
+        tweet_user_dict["birth_date"] = str(tweet_user_dict["birth_date"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        print(results)
+        return tweet
+        
 
 ### Show a Tweet
 @app.get(
